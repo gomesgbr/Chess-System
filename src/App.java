2222f9f514da05f5dd72d19b0400c63dfd0202f7
Gabriel Gomes <gabrielg299@gmail.com>
2022-07-18 09:13:20 -0300

    Handling exceptions and clear terminal

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.chessException;
 import chess.chessMatch;
 import chess.chessPiece;
 import chess.chessPosition;
@@ -11,16 +13,27 @@ public class App {
         Scanner sc = new Scanner(System.in);
         chessMatch chessMatch = new chessMatch();
         while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.println("Source: ");
-            chessPosition source = UI.readChessPosition(sc);
 
-            System.out.println("Target: ");
-            chessPosition target = UI.readChessPosition(sc);
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                chessPosition source = UI.readChessPosition(sc);
 
-            chessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                System.out.println("Target: ");
+                chessPosition target = UI.readChessPosition(sc);
 
+                chessPiece capturedPiece = chessMatch.performChessMove(source, target);
+ 
+            }catch(chessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            
         }
         
     }
